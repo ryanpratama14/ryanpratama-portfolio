debce656e27e6dd0e89ad2c7090b79d92aae84b3
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-06-01 18:34:28 +0300

    adjusted

--- src/app/components/AnimatedUnderlinedText.tsx
+++ src/app/components/AnimatedUnderlinedText.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 type Props = {
   text?: string;
+  className?: string;
 };
 
 export default function AnimatedUnderlinedText({
   text,
+  className,
 }: Props): React.JSX.Element {
   return (
     <div className="relative w-fit">
-      <h1>{text}</h1>
+      <h5 className={`${className}`}>{text}</h5>
       <div className="animate absolute -bottom-1 h-1 themedBg2nd w-0 group-hover:w-full rounded-md themedShadowGlowed" />
     </div>
   );
--- src/app/components/sections/Main.tsx
+++ src/app/components/sections/Main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AnimatedText from "../AnimatedText";
 import { Icon } from "@iconify/react";
 import { skillsData } from "@/app/constants/constants";
+import AnimatedUnderlinedText from "../AnimatedUnderlinedText";
 
 export default function Main(): React.JSX.Element {
   return (
@@ -11,13 +12,15 @@ export default function Main(): React.JSX.Element {
           <label className="xl:text-xl">Hi! My name is</label>
           <h1 className="-translate-x-[0.2rem]">Ryan</h1>
         </div>
-        <AnimatedText text="I create digital products for the internet" />
-        <p className="w-full lg:w-[75%] xl:w-[50%]">
-          A front-end developer specializing in building intuitive web
-          applications with engaging user interfaces. I utilize the React.js
-          framework to develop efficient, scalable and maintainable frontend
-          applications.
-        </p>
+        <div className="flex flex-col gap-3">
+          <AnimatedText text="I create digital products for the internet" />
+          <p className="w-full lg:w-[75%] xl:w-[50%]">
+            A front-end developer specializing in building intuitive web
+            applications with engaging user interfaces. I utilize the React.js
+            framework to develop efficient, scalable and maintainable frontend
+            applications.
+          </p>
+        </div>
         <div className="flex flex-col gap-3 w-full lg:w-[75%] xl:w-[50%]">
           <h5 className="italic">Tech Stacks</h5>
           <ul className="flex flex-wrap gap-3 themedText text-sm">
