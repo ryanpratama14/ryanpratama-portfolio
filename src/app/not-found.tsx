b79bbf8a778a4284848619cbd06a3bbd90dc8357
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-06-12 02:21:22 +0300

    adjusted

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,19 +6,31 @@ import Contact from "./components/sections/Contact";
 
 export default function NotFound(): React.JSX.Element {
   return (
-    <section className="flex flex-col justify-center items-center">
+    <main>
       <div className="min-h-screen flex flex-col justify-center items-center gap-8">
-        <Icon
-          icon="ooui:article-not-found-ltr"
-          className="text-turquoise"
-          width={200}
-        />
-        <h2>Ooops, page not found</h2>
+        <div className="flex gap-2">
+          <Icon
+            icon="ooui:article-not-found-ltr"
+            className="text-turquoise"
+            width={100}
+          />
+          <Icon
+            icon="ooui:article-not-found-ltr"
+            className="text-bluedarker"
+            width={100}
+          />
+          <Icon
+            icon="ooui:article-not-found-ltr"
+            className="text-blue"
+            width={100}
+          />
+        </div>
+        <h3>Ooops, page not found</h3>
         <Link className="btn-nav px-6 py-2" href="/">
           Back to Homepage
         </Link>
       </div>
       <Contact />
-    </section>
+    </main>
   );
 }
