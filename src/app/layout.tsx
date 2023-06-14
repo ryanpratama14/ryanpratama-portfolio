7ebb0d18b7fbc9f76e89f2122941f481ff6abddb
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-06-14 19:56:19 +0300

    adjsuted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import Navbar from "../components/navbar/Navbar";
 import ScrollToTop from "../components/ScrollToTop";
 import Footer from "../components/Footer";
 import Providers from "./template";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Ryan's Next App",
