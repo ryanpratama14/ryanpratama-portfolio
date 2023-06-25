82ebf5b7207ff24c27e1a4256e567835e9066d1a
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-06-25 22:05:50 +0300

    adjusted

--- src/components/TransitionEffect.tsx
+++ src/components/TransitionEffect.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 export default function TransitionEffect(): React.JSX.Element {
   return (
     <Fragment>
-      {/* <motion.div
+      <motion.div
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "0%" }}
         transition={{ duration: 1, ease: "easeIn" }}
@@ -21,7 +21,7 @@ export default function TransitionEffect(): React.JSX.Element {
         animate={{ x: "0%", width: "0%" }}
         transition={{ delay: 0.4, duration: 1, ease: "easeIn" }}
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-turquoise"
-      /> */}
+      />
     </Fragment>
   );
 }
