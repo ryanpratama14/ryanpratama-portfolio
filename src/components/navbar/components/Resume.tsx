323c6edbd991c6f3fb03134ca2140f802f9b1dab
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-06-30 01:07:05 +0300

    adjusted

--- src/components/navbar/components/Resume.tsx
+++ src/components/navbar/components/Resume.tsx
@@ -11,15 +11,15 @@ export default function Resume(): React.JSX.Element {
           icon="pepicons-pencil:cv-circle"
           className="text-3xl md:text-4xl rotate-[16deg] group-hover:rotate-0 group-hover:scale-110 transition-transform duration-300"
         />
-        <div className="relative h-full flex items-center">
-          <span className="cursor-pointer z-[2] drop-shadow label text-lg">
+        <header className="relative h-full flex items-center">
+          <span className="cursor-pointer z-[2] drop-shadow text-lg font-semibold">
             Resume
           </span>
           <div className="-skew-x-[16deg] absolute top-0 w-full h-full flex justify-center items-center z-[1]">
             <div className="w-6 h-full bg-turquoise group-hover:bg-bluedarker animate" />
             <div className="w-2 h-full bg-bluedarker group-hover:bg-white animate" />
           </div>
-        </div>
+        </header>
       </Menu.Button>
       <Transition
         as={Fragment}
