dd152f846183d69dca48cace7da555d1a692f0c9
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-07-01 00:01:44 +0300

    adjusted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,10 @@
 import "@/styles/globals.css";
-import { Poppins, Montserrat } from "next/font/google";
 import { Metadata } from "next";
 
 // components
 import ReduxProvider from "@/global/ReduxProvider";
 import Navbar from "@/components/navbar/Navbar";
 import ScrollToTop from "@/components/ScrollToTop";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  display: "swap",
-  variable: "--font-poppins",
-});
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  display: "swap",
-  variable: "--font-montserrat",
-});
 
 export const metadata: Metadata = {
   title: "Ryan's Next App",
@@ -32,7 +17,7 @@ type Props = {
 
 export default function RootLayout({ children }: Props): React.JSX.Element {
   return (
-    <html lang="en" className={`${poppins.variable} ${montserrat.variable}`}>
+    <html lang="en">
       <body>
         <ReduxProvider>
           <Navbar />
