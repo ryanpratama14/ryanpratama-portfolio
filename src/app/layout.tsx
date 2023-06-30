dd89107d4f5057891ba2ec7622010e3c0330264a
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-07-01 00:38:05 +0300

    adjusted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,6 @@ import ReduxProvider from "@/global/ReduxProvider";
 import Navbar from "@/components/navbar/Navbar";
 import ScrollToTop from "@/components/ScrollToTop";
 
-export const metadata: Metadata = {
-  title: "Ryan's Next App",
-  description: "Welcome to: Ryan's Portfolio",
-};
-
-type Props = {
-  children: React.ReactNode;
-};
-
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -29,6 +20,15 @@ const montserrat = Montserrat({
   display: "swap",
   variable: "--font-montserrat",
 });
+
+export const metadata: Metadata = {
+  title: "Ryan's Next App",
+  description: "Welcome to: Ryan's Portfolio",
+};
+
+type Props = {
+  children: React.ReactNode;
+};
 
 export default function RootLayout({ children }: Props): React.JSX.Element {
   return (
