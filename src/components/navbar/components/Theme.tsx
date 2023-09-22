3669d9e9c7dec12ec2733f35281169a5388ea8ef
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-09-23 03:46:29 +0700

    adjusted

--- src/components/navbar/components/Theme.tsx
+++ src/components/navbar/components/Theme.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import useLocalStorage from "@/hooks/useLocalStorage";
 import Iconify from "@/components/Iconify";
+import useLocalStorage from "@/hooks/useLocalStorage";
 
 export default function Theme(): React.JSX.Element {
   const [theme, setTheme] = useLocalStorage("theme", "dark");
