682a5bfdab97641683f5fc735415e3e71f8e7611
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-09-23 04:18:53 +0700

    adjsuted

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function useLocalStorage(key: string, initialValue: any) {
+  const [state, setState] = useState(() => {
+    try {
+      const value = window.localStorage.getItem(key);
+      return value ? JSON.parse(value) : initialValue;
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  const setValue = (value: any) => {
+    try {
+      const valueToStore = value instanceof Function ? value(state) : value;
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      setState(value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [state, setValue];
+}
