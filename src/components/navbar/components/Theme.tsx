bc3209bffc307d97269b20461588ca12758eb5ba
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-09-23 04:00:52 +0700

    adjusted

--- src/components/navbar/components/Theme.tsx
+++ src/components/navbar/components/Theme.tsx
@@ -1,31 +1,31 @@
-import { useEffect } from "react";
-import Iconify from "@/components/Iconify";
-import { useLocalStorage } from "@/hooks/useLocalStorage";
+// import { useEffect } from "react";
+// import Iconify from "@/components/Iconify";
+// import { useLocalStorage } from "@/hooks/useLocalStorage";
 
-export default function Theme(): React.JSX.Element {
-  const [theme, setTheme] = useLocalStorage("theme", "dark");
+// export default function Theme(): React.JSX.Element {
+//   const [theme, setTheme] = useLocalStorage("theme", "dark");
 
-  useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [theme]);
+//   useEffect(() => {
+//     if (theme === "dark") {
+//       document.documentElement.classList.add("dark");
+//     } else {
+//       document.documentElement.classList.remove("dark");
+//     }
+//   }, [theme]);
 
-  return (
-    <section
-      className="flex justify-center items-center cursor-pointer themedText"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-    >
-      <Iconify
-        width={25}
-        icon={`${
-          theme === "dark"
-            ? "material-symbols:light-mode"
-            : "material-symbols:dark-mode-outline-rounded"
-        }`}
-      />
-    </section>
-  );
-}
+//   return (
+//     <section
+//       className="flex justify-center items-center cursor-pointer themedText"
+//       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+//     >
+//       <Iconify
+//         width={25}
+//         icon={`${
+//           theme === "dark"
+//             ? "material-symbols:light-mode"
+//             : "material-symbols:dark-mode-outline-rounded"
+//         }`}
+//       />
+//     </section>
+//   );
+// }
