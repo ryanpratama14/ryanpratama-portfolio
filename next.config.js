28946f6aa16ff690bcfd85e528a167ea9bd440ee
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-11-02 10:21:28 +0700

    adjusted

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-});
 const nextConfig = {};
 
-module.exports = withPWA({
-  nextConfig,
-});
+module.exports = nextConfig;
