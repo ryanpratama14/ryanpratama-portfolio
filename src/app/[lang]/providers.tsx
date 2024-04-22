caf6c5ad265c1e4417303300c3a4ed8add80af6f
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-04-22 07:26:48 +0700

    update: package.json

--- src/app/[lang]/providers.tsx
+++ src/app/[lang]/providers.tsx
@@ -2,10 +2,19 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const createQueryClient = () => new QueryClient();
+
+let clientQueryClientSingleton: QueryClient | undefined = undefined;
+
+const getQueryClient = () => {
+  if (typeof window === "undefined") return createQueryClient();
+  return (clientQueryClientSingleton ??= createQueryClient());
+};
 
 type Props = { children: React.ReactNode };
 
 export default function Providers({ children }: Props) {
+  const queryClient = getQueryClient();
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
