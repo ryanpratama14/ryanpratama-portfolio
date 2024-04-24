f3eb70fdd75db972b3c2f3f0036dcae29e318be7
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-04-24 17:54:26 +0700

    fix: internationalization cookie issue 3

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,5 +4,5 @@ import type { Lang } from "@/types";
 import { cookies } from "next/headers";
 
 export const setCookieLang = async (lang: Lang) => {
-  cookies().set("lang", lang);
+  cookies().set("lang", lang, { httpOnly: true, sameSite: "lax" });
 };
