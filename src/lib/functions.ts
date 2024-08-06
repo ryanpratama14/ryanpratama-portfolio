34c233f8f3fc4920e4c787160ed6d304a74119b8
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-08-06 16:09:24 +0700

    adjust: layout

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,19 +1,15 @@
 import { type ClassValue, clsx } from "clsx";
-import dayjs from "dayjs";
 import { twMerge } from "tailwind-merge";
 import { DEFAULT_LANGUAGE } from "./internationalization";
 
-export const isClient = typeof window !== "undefined";
+const currentTime = new Date().toLocaleTimeString(DEFAULT_LANGUAGE.locale, { hour: "2-digit", minute: "2-digit", second: "2-digit" });
+const isClient = typeof window !== "undefined";
+
 export const cn = (...inputs: ClassValue[]): string => twMerge(clsx(inputs));
 export const loadToTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
 export const copyData = <T>(data: T): T => structuredClone(data);
 export const formatDate = (date: Date, locale: string) => date.toLocaleDateString(locale, { month: "short", year: "numeric" });
-
-export const consoleError = (error: string) => {
-  console.error(
-    `❌ ${dayjs().toDate().toLocaleTimeString(DEFAULT_LANGUAGE.locale, { hour: "2-digit", minute: "2-digit", second: "2-digit" })} 👉 ${error}`,
-  );
-};
+export const consoleError = (error: string) => console.error(`❌ ${currentTime} 👉 ${error}`);
 
 export const getBaseUrl = () => {
   if (isClient) return window.location.origin;
