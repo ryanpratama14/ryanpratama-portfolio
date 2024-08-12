d41cb516b12fed424a44f80ad80130178cf9b49f
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-08-13 00:01:54 +0700

    adjust: scroll-bar

--- src/components/sections/featured-projects.tsx
+++ src/components/sections/featured-projects.tsx
@@ -19,25 +19,27 @@ export default function FeaturedProjects({ s }: Props) {
         modules={[Autoplay, Mousewheel, Scrollbar]}
         mousewheel={{ forceToAxis: true }}
         autoplay={{ delay: 2500, pauseOnMouseEnter: true, stopOnLastSlide: false, disableOnInteraction: false }}
-        scrollbar={{ draggable: true }}
+        scrollbar={{ draggable: true, el: ".swiper-scrollbar" }}
         className="w-full"
         simulateTouch={false}
         spaceBetween={10}
-        slidesPerView={1.2}
-        breakpoints={{ 768: { slidesPerView: 2.2 }, 1024: { slidesPerView: 2.4 }, 1280: { slidesPerView: 3.2 } }}
+        slidesPerView={1.15}
+        breakpoints={{ 768: { slidesPerView: 2.15 }, 1024: { slidesPerView: 2.65 }, 1280: { slidesPerView: 3.15 } }}
       >
         {PROJECTS.map((e) => {
           return (
             <SwiperSlide key={e.title}>
-              <section className="aspect-[4/5] relative group overflow-hidden rounded-md">
-                <section className="rounded-md z-10 absolute left-0 top-0 w-full group-hover:h-full h-0 opacity-0 group-hover:opacity-100 bg-black/90 animate-longer flex flex-col items-center justify-center gap-1.5 px-4 lg:px-8">
-                  <small className="text-pretty">{e.desc}</small>
-                  <ul>
-                    {e.lists.map((list) => (
-                      <li key={list}>{list}</li>
-                    ))}
-                  </ul>
-                  <Link target="_blank" href={e.href} className="box-button mt-2">
+              <section className="aspect-[4/5] relative group overflow-hidden rounded-md mb-2">
+                <section className="rounded-md z-10 absolute top-0 left-0 size-full opacity-0 group-hover:opacity-100 bg-black/90 animate flex flex-col items-center justify-center gap-1.5 px-4 lg:px-8">
+                  <section className="group-hover:translate-y-0 -translate-y-6 animate">
+                    <small className="text-pretty">{e.desc}</small>
+                    <ul>
+                      {e.lists.map((list) => (
+                        <li key={list}>{list}</li>
+                      ))}
+                    </ul>
+                  </section>
+                  <Link target="_blank" href={e.href} className="box-button mt-2 group-hover:translate-y-0 translate-y-6 !duration-700">
                     {s.SECTIONS.visitProject}
                   </Link>
                 </section>
@@ -46,6 +48,7 @@ export default function FeaturedProjects({ s }: Props) {
             </SwiperSlide>
           );
         })}
+        <div className="swiper-scrollbar" />
       </Swiper>
     </article>
   );
