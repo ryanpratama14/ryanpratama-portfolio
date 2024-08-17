d6716687d1b6c557804050f2d1231683a2ce1e86
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-08-17 13:52:24 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,7 +22,7 @@ export const middleware = (req: NextRequest) => {
   const lang = getLangFromPath(path) ?? storedLang ?? getLang(req);
   const newUrl = new URL(`/${lang}${path.startsWith("/") ? "" : "/"}${path}`, req.url);
   const response = isLangMissing(path) ? NextResponse.redirect(newUrl) : NextResponse.next();
-  if (storedLang && !validateLang(storedLang)) response.cookies.set("lang", getLang(req), { httpOnly: true, sameSite: "lax" });
+  if (!validateLang(lang)) response.cookies.set("lang", getLang(req), { httpOnly: true, sameSite: "lax" });
   return response;
 };
 
