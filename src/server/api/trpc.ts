6943f27b245be7fbf77735f1ee65242a930d4ea9
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-08-20 10:49:02 +0700

    add: trpc timing middleware

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -15,4 +15,17 @@ const t = initTRPC.context<typeof createTRPCContext>().create({
 
 export const createCallerFactory = t.createCallerFactory;
 export const createTRPCRouter = t.router;
-export const publicProcedure = t.procedure;
+
+const timingMiddleware = t.middleware(async ({ next, path }) => {
+  const start = Date.now();
+  if (t._config.isDev) {
+    const waitMs = Math.floor(Math.random() * 400) + 100;
+    await new Promise((resolve) => setTimeout(resolve, waitMs));
+  }
+  const result = await next();
+  const end = Date.now();
+  console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
+  return result;
+});
+
+export const publicProcedure = t.procedure.use(timingMiddleware);
