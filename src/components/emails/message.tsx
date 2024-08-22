7d1bd4ca789b415ad854c9b9d715468528345a2a
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-08-22 23:53:46 +0700

    adjust: emails

--- src/components/emails/message.tsx
+++ src/components/emails/message.tsx
@@ -6,12 +6,17 @@ export default function Message({ name, email, message, lang }: MessageInput) {
     <Html>
       <Head />
       <Tailwind>
-        <Body className="bg-white flex items-center justify-center py-12 font-sans">
-          <Container className="border border-solid border-[#eaeaea] rounded w-[36rem] px-6 *:text-xl">
-            <Text>Name: {name}</Text>
-            <Text>Email: {email}</Text>
-            <Text className="whitespace-pre-line">Message: {message}</Text>
-            <Text>Lang: {lang}</Text>
+        <Body className="font-sans">
+          <Container>
+            <Text>
+              Name: {name}
+              <br />
+              Email: {email}
+              <br />
+              <span className="whitespace-pre-line">Message: {message}</span>
+              <br />
+              Lang: {lang}
+            </Text>
           </Container>
         </Body>
       </Tailwind>
