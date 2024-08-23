7e0f4b0d88d8fe85ec0f7f6bf7acece7fafd69cd
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-08-24 03:09:52 +0700

    adjust: lang switcher

--- src/components/lang-switcher.tsx
+++ src/components/lang-switcher.tsx
@@ -5,27 +5,41 @@ import { useLanguageHelper } from "@/internationalization/functions";
 import { cn } from "@/lib/utils";
 import type { Lang } from "@/types";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { Fragment } from "react";
 
 type Props = { lang: Lang };
 
 export default function LangSwitcher({ lang }: Props) {
+  const path = usePathname();
+  const router = useRouter();
+  const { changeLang } = useLanguageHelper();
+
   return (
     <section className="flex">
       {LANGUAGE_OPTIONS.map(({ lang: targetLang, t: { s }, flag, label }) => {
         const isActive = lang === targetLang;
+        const href = changeLang(targetLang, path);
+
         return (
-          <Link
-            className={cn("text-2xl leading-3 px-1 py-1.5 rounded-md border-1 border-transparent", {
-              "bg-graybg border-graydarker shadow-xl": isActive,
-            })}
-            key={targetLang}
-            href={useLanguageHelper().changeLang(targetLang, usePathname())}
-            type="button"
-          >
-            <span className="sr-only">{`[${label} — ${targetLang}]: ${s.PERSONAL_DATA.fullName}. ${s.PERSONAL_DATA.softwareEngineer}. ${s.PERSONAL_DATA.summary}`}</span>
-            {flag}
-          </Link>
+          <Fragment key={targetLang}>
+            <Link href={href} className="sr-only">
+              {label}
+            </Link>
+            <button
+              className={cn("text-2xl leading-3 px-1 py-1.5 rounded-md border-1 border-transparent", {
+                "bg-graybg border-graydarker shadow-xl": isActive,
+              })}
+              onClick={() => {
+                router.push(href);
+                router.refresh();
+              }}
+              type="button"
+            >
+              <span className="sr-only">{`[${label} — ${targetLang}]: ${s.PERSONAL_DATA.fullName}. ${s.PERSONAL_DATA.softwareEngineer}. ${s.PERSONAL_DATA.summary}`}</span>
+              {flag}
+            </button>
+          </Fragment>
         );
       })}
     </section>
