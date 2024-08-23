7aff61837fc79a289b0b2eca6ce427d77337f898
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-08-24 03:15:27 +0700

    adjust: middleware

--- src/components/lang-switcher.tsx
+++ src/components/lang-switcher.tsx
@@ -5,14 +5,12 @@ import { useLanguageHelper } from "@/internationalization/functions";
 import { cn } from "@/lib/utils";
 import type { Lang } from "@/types";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { Fragment } from "react";
+import { usePathname } from "next/navigation";
 
 type Props = { lang: Lang };
 
 export default function LangSwitcher({ lang }: Props) {
   const path = usePathname();
-  const router = useRouter();
   const { changeLang } = useLanguageHelper();
 
   return (
@@ -22,24 +20,17 @@ export default function LangSwitcher({ lang }: Props) {
         const href = changeLang(targetLang, path);
 
         return (
-          <Fragment key={targetLang}>
-            <Link href={href} className="sr-only">
-              {label}
-            </Link>
-            <button
-              className={cn("text-2xl leading-3 px-1 py-1.5 rounded-md border-1 border-transparent", {
-                "bg-graybg border-graydarker shadow-xl": isActive,
-              })}
-              onClick={() => {
-                router.push(href);
-                router.refresh();
-              }}
-              type="button"
-            >
-              <span className="sr-only">{`[${label} — ${targetLang}]: ${s.PERSONAL_DATA.fullName}. ${s.PERSONAL_DATA.softwareEngineer}. ${s.PERSONAL_DATA.summary}`}</span>
-              {flag}
-            </button>
-          </Fragment>
+          <Link
+            key={targetLang}
+            className={cn("text-2xl leading-3 px-1 py-1.5 rounded-md border-1 border-transparent", {
+              "bg-graybg border-graydarker shadow-xl": isActive,
+            })}
+            href={href}
+            type="button"
+          >
+            <span className="sr-only">{`[${label} — ${targetLang}]: ${s.PERSONAL_DATA.fullName}. ${s.PERSONAL_DATA.softwareEngineer}. ${s.PERSONAL_DATA.summary}`}</span>
+            {flag}
+          </Link>
         );
       })}
     </section>
