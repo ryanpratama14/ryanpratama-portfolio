a0f4d1c5a5e0ee0b803a88fff430e7358c1625b3
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-08-25 00:10:15 +0700

    adjust: metadata

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -32,7 +32,7 @@ export const getMetadata = (lang: Lang): Metadata => {
     openGraph: {
       title: { default: title, template: `%s | ${title}` },
       description,
-      url,
+      url: `${url}/${lang}`,
       siteName: title,
       locale: locale.split("-").join("_"),
       type: "website",
