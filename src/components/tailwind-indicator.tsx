1d4f4a4a18b82b6158cb740e12ab71fcde29f667
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-09-18 22:34:04 +0700

    adjust: indicator

--- src/components/tailwind-indicator.tsx
+++ src/components/tailwind-indicator.tsx
@@ -26,11 +26,12 @@ export default function TailwindIndicator() {
   return (
     <div className="fixed bottom-5 left-5 z-50 flex items-center gap-1.5 rounded-md bg-white px-2.5 py-1 text-sm font-bold text-black shadow">
       <span className="sm:hidden">XS</span>
-      <span className="hidden sm:inline md:hidden">SM</span>
-      <span className="hidden md:inline lg:hidden">MD</span>
-      <span className="hidden lg:inline xl:hidden">LG</span>
-      <span className="hidden xl:inline 2xl:hidden">XL</span>
-      <span className="hidden 2xl:inline">2XL</span>
+      <span className="hidden sm:block md:hidden">SM</span>
+      <span className="hidden md:block lg:hidden">MD</span>
+      <span className="hidden lg:block xl:hidden">LG</span>
+      <span className="hidden xl:block 2xl:hidden">XL</span>
+      <span className="hidden 2xl:block 3xl:hidden">2XL</span>
+      <span className="hidden 3xl:block">3XL</span>
       <span className="font-medium">
         {width.toLocaleString()} x {height.toLocaleString()}
       </span>
