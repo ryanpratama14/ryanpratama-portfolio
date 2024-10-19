d569097fa43f2c85f71e8f493d977252e9a59eac
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-10-19 13:54:39 +0700

    adjust: packages

--- src/server/api/schema/index.ts
+++ src/server/api/schema/index.ts
@@ -2,15 +2,15 @@ import { LANGS } from "@/internationalization";
 import type { DictionaryStatic } from "@/types";
 import { z } from "zod";
 
-export class schema {
-  static email = class {
-    static message = (s: DictionaryStatic) => {
+export const schema = {
+  email: {
+    message: (s: DictionaryStatic) => {
       return z.object({
         name: z.string().min(1, s.MESSAGE.name.error),
         email: z.string().email(s.MESSAGE.email.error),
         message: z.string().min(5, s.MESSAGE.message.error),
         lang: z.enum(LANGS),
       });
-    };
-  };
-}
+    },
+  },
+};
