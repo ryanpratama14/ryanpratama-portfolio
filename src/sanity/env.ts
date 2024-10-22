0a1599a226132a03c08273bc9779e46d572185fa
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-10-22 23:22:45 +0700

    add: sanity CMS

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -6,7 +6,7 @@ export const dataset = assertValue(
 );
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "tawhr7jk",
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "rn9dt79n",
   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
 );
 
