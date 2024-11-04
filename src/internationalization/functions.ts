50e544ce4368de4db2efb12b04326d8a48ebf157
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-11-04 08:41:51 +0700

    adjust: layout

--- src/internationalization/functions.ts
+++ src/internationalization/functions.ts
@@ -13,7 +13,8 @@ const useLang = (lang: Lang) => {
   const currentTime = new Date().toLocaleTimeString(locale, { hour: "2-digit", minute: "2-digit", second: "2-digit" });
   const splittedLocale = locale.split("-").join("_");
 
-  const formatMonth = (date: Date) => date.toLocaleDateString(locale, { month: "short", year: "numeric" });
+  const formatMonth = (date: Date) => date.toLocaleDateString(locale, { month: isJapanese ? "short" : "2-digit", year: "numeric" });
+
   const formatDate = (date: Date) => date.toLocaleDateString(locale, { day: "numeric", month: "long", year: "numeric" });
   const formatCurrency = (amount: number) => new Intl.NumberFormat(locale, { style: "currency", currency }).format(amount);
   const formatCounter = (counter: string) => (isJapanese ? counter : ` ${counter}`);
