27f74fd7d44b1453a6d0c042d10f7a0898e2d101
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-12-13 19:39:14 +0800

    adjust: packages

--- src/sanity/lib/default-document-node.ts
+++ src/sanity/lib/default-document-node.ts
@@ -1,19 +1,17 @@
-import { getBaseUrl } from "@/app/urls";
+import { PATHS, getBaseUrl } from "@/app/urls";
 import type { SanityDocument } from "sanity";
 import { Iframe } from "sanity-plugin-iframe-pane";
 import type { DefaultDocumentNodeResolver } from "sanity/structure";
 
 export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, { schemaType }) => {
-  if (schemaType === "post") {
-    return S.document().views([
-      S.view.form(),
-      S.view
-        .component(Iframe)
-        .options({
-          url: (doc: SanityDocument & { slug?: { current: string } }) =>
-            doc?.slug?.current ? `${getBaseUrl()}/blog/${doc.slug.current}` : getBaseUrl(),
-        })
-        .title("Preview"),
-    ]);
-  }
+  return S.document().views([
+    S.view.form(),
+    S.view
+      .component(Iframe)
+      .options({
+        url: (doc: SanityDocument & { slug?: { current: string } }) =>
+          doc?.slug?.current ? `${getBaseUrl()}${PATHS[schemaType as keyof typeof PATHS]}/${doc.slug.current}` : getBaseUrl(),
+      })
+      .title("Preview"),
+  ]);
 };
