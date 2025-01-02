995efb73bc360f27993ad51a9b81968769e493bf
Ryan Pratama <ru.ryanpratama@gmail.com>
2025-01-02 23:41:42 +0800

    adjust: packages

--- src/app/[lang]/certification/page.tsx
+++ src/app/[lang]/certification/page.tsx
@@ -1,8 +1,15 @@
+import { getMetadata } from "@/app/metadata";
 import CertificationCards from "@/components/certification-cards";
 import { useLang } from "@/internationalization/functions";
 import type { Lang } from "@/types";
+import type { Metadata } from "next";
 
 type Props = { params: Promise<{ lang: Lang }> };
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const { s } = useLang((await params).lang);
+  return await getMetadata({ title: s.MENUS.certifications });
+};
 
 export default async function CertificationPage({ params }: Props) {
   const { lang } = await params;
--- src/app/[lang]/resume/page.tsx
+++ src/app/[lang]/resume/page.tsx
@@ -1,10 +1,17 @@
+import { getMetadata } from "@/app/metadata";
 import { URLS } from "@/app/urls";
 import Container from "@/components/container";
 import PdfViewer from "@/components/pdf-viewer";
 import { useLang } from "@/internationalization/functions";
 import type { Lang } from "@/types";
+import type { Metadata } from "next";
 
 type Props = { params: Promise<{ lang: Lang }> };
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const { s } = useLang((await params).lang);
+  return await getMetadata({ title: s.SECTIONS.resume });
+};
 
 export default async function ResumePage({ params }: Props) {
   const { lang } = await params;
