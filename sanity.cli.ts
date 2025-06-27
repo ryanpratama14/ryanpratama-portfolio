cff4dd93594fbdd355bed43d7f891a089380ed90
Ryan Pratama <ru.ryanpratama@gmail.com>
2025-06-27 18:59:41 +0700

    adjust: packages

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,7 +1,6 @@
 import { defineCliConfig } from "sanity/cli";
-import { env } from "@/env";
 
-const projectId = env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineCliConfig({ api: { projectId, dataset } });
