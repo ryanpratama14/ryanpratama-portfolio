050150d18340cae888a6b7de2b0dab995beaa60a
Ryan Pratama <ru.ryanpratama@gmail.com>
2025-07-04 13:25:36 +0700

    adjust

--- src/server/orpc.ts
+++ src/server/orpc.ts
@@ -1,6 +1,5 @@
 import { createORPCClient } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
-import { BatchLinkPlugin } from "@orpc/client/plugins";
 import type { RouterClient } from "@orpc/server";
 import { ENDPOINTS } from "@/app/urls";
 import type { router } from "./router";
@@ -10,7 +9,6 @@ declare global {
 }
 
 const link = new RPCLink({
-  plugins: [new BatchLinkPlugin({ groups: [{ condition: () => true, context: {} }] })],
   url: () => {
     if (typeof window === "undefined") {
       throw new Error("RPCLink is not allowed on the server side.");
