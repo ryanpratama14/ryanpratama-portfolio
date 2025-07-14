500ed18748f4b891c0a9b5d3409a349c86610c8a
Ryan Pratama <ru.ryanpratama@gmail.com>
2025-07-15 05:34:31 +0700

    adjust

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,24 @@
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { HEADERS } from "@/app/urls";
-import { getLangFromPath, isLangMissing, validateMatchedLang } from "@/internationalization/functions";
+import { DEFAULT_LANG, LANGS } from "@/internationalization";
+import { getLangFromPath, isLangMissing, validateLang, validateMatchedLang } from "@/internationalization/functions";
+
+const getLang = (req: NextRequest) => {
+  const headers = new Headers(req.headers);
+  const acceptLanguage = headers.get("accept-language");
+  if (acceptLanguage) headers.set("accept-language", acceptLanguage.replaceAll("_", "-"));
+  const headersObject = Object.fromEntries(headers.entries());
+  const languages = new Negotiator({ headers: headersObject }).languages();
+  if (languages.includes("*")) return DEFAULT_LANG;
+  return validateMatchedLang(match(languages, LANGS, DEFAULT_LANG));
+};
 
 export const middleware = (req: NextRequest) => {
   const path = req.nextUrl.pathname;
-  const lang = getLangFromPath(path) ?? validateMatchedLang(req.cookies.get("lang")?.value);
+  const lang = getLangFromPath(path) ?? validateLang(req.cookies.get("lang")?.value) ?? getLang(req);
   if (isLangMissing(path)) return NextResponse.redirect(new URL(`/${lang}${path.startsWith("/") ? "" : "/"}${path}`, req.url));
   const res = NextResponse.next();
   res.headers.set(HEADERS.lang, lang);
