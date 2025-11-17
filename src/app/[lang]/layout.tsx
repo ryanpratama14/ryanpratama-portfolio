80db2d6ff2042efff3b68806b02a13236b0d3bee
Ryan Pratama <ru.ryanpratama@gmail.com>
2025-11-17 22:36:54 +0700

    adjust

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,11 @@
+import { draftMode } from "next/headers";
+import { VisualEditing } from "next-sanity/visual-editing";
+import { Fragment } from "react";
 import Container from "@/components/container";
+import { DisableDraftMode } from "@/components/disable-draft-mode";
 import { env } from "@/env";
 import { getLang } from "@/internationalization/functions";
+import { SanityLive } from "@/sanity/lib/live";
 import type { Lang } from "@/types";
 import Message from "./(home)/components/message";
 import Profile from "./(home)/components/profile";
@@ -9,22 +14,31 @@ type Props = { children: React.ReactNode; params: Promise<{ lang: string }> };
 
 export default async function RootLayout({ params, children }: Props): Promise<React.JSX.Element> {
   const { lang, s, d, isDefaultLang, formatDate } = getLang((await params).lang as Lang);
-
+  const { isEnabled: isDraftMode } = await draftMode();
   return (
-    <main className="flex flex-col gap-4 main-padding">
-      <Profile s={s} lang={lang} isDefaultLang={isDefaultLang} />
-      {children}
-      <Message s={s} lang={lang} />
-      <Container title={d.updatedOn(formatDate(new Date()))} />
-      <iframe
-        title="Spotify"
-        src={env.SPOTIFY_TRACK_URL}
-        width="100%"
-        allowFullScreen
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-        className="wrapper rounded-sm"
-      />
-    </main>
+    <Fragment>
+      <SanityLive />
+      {isDraftMode && (
+        <Fragment>
+          <DisableDraftMode />
+          <VisualEditing />
+        </Fragment>
+      )}
+      <main className="flex flex-col gap-4 main-padding">
+        <Profile s={s} lang={lang} isDefaultLang={isDefaultLang} />
+        {children}
+        <Message s={s} lang={lang} />
+        <Container title={d.updatedOn(formatDate(new Date()))} />
+        <iframe
+          title="Spotify"
+          src={env.SPOTIFY_TRACK_URL}
+          width="100%"
+          allowFullScreen
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          className="wrapper rounded-sm"
+        />
+      </main>
+    </Fragment>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,20 +7,15 @@ import "@/server/orpc.server";
 
 import { GoogleTagManager } from "@next/third-parties/google";
 import { GeistSans } from "geist/font/sans";
-import { draftMode } from "next/headers";
-import { VisualEditing } from "next-sanity/visual-editing";
 import NextTopLoader from "nextjs-toploader";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
-import { Fragment } from "react";
 import { Toaster } from "sonner";
 import { getMetadata } from "@/app/metadata";
-import { DisableDraftMode } from "@/components/disable-draft-mode";
 import ScreenSizeIndicator from "@/components/screen-size-indicator";
 import { env } from "@/env";
 import { LANGS } from "@/internationalization";
 import { getHeaders } from "@/lib/actions";
 import { Providers } from "@/lib/tanstack-query/providers";
-import { SanityLive } from "@/sanity/lib/live";
 import { COLORS } from "@/styles";
 
 export const generateStaticParams = async () => LANGS.map((lang) => ({ lang }));
@@ -29,19 +24,12 @@ export const generateMetadata = async () => await getMetadata({});
 type Props = { children: React.ReactNode };
 
 export default async function RootLayout({ children }: Props) {
-  const [{ lang }, { isEnabled: isDraftMode }] = await Promise.all([getHeaders(), draftMode()]);
+  const [{ lang }] = await Promise.all([getHeaders()]);
 
   return (
     <html lang={lang} className={GeistSans.variable}>
       <GoogleTagManager gtmId={env.NEXT_PUBLIC_GTM_ID} />
       <body className="text-white bg-black font-sans">
-        <SanityLive />
-        {isDraftMode && (
-          <Fragment>
-            <DisableDraftMode />
-            <VisualEditing />
-          </Fragment>
-        )}
         <NuqsAdapter>
           <Providers>
             {children}
