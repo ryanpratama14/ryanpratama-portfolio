2948d680f0ee7214f189410659a8e203b637ba94
Ryan Pratama <ru.ryanpratama@gmail.com>
2026-05-10 11:32:24 +0900

    adjust

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -2,12 +2,8 @@ import { defineConfig } from "oxlint";
 
 export default defineConfig({
   $schema: "./node_modules/oxlint/configuration_schema.json",
-  plugins: ["typescript", "unicorn", "oxc"],
-  categories: {
-    correctness: "error",
-  },
+  plugins: ["typescript", "unicorn", "oxc", "nextjs", "jsx-a11y", "eslint", "react"],
+  categories: { correctness: "error" },
   rules: {},
-  env: {
-    builtin: true,
-  },
+  env: { builtin: true },
 });
