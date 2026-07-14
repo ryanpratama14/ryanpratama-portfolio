a6f205dcd50850a225558dcb7fd200ec4c2fd9c5
Ryan Pratama <ru.ryanpratama@gmail.com>
2026-07-14 21:07:35 +0900

    adjust

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import "./src/env";
 const config: NextConfig = {
   reactCompiler: true,
   images: { remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }] },
+  experimental: { useTypeScriptCli: true },
 };
 
 export default config;
